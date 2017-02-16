3e2479508d86c118192005726fad72b969293b5b
lesnikovskiy <lesnikovski@gmail.com>
2017-02-16 23:18:22 +0200

    two way databinding

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {RouterModule, ActivatedRouteSnapshot} from "@angular/router";
 
 import {
@@ -22,6 +23,8 @@ import {appRoutes} from "./routes";
 @NgModule({
     imports: [
         BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
         RouterModule.forRoot(appRoutes)
     ],
     declarations: [
--- app/events/create-event.component.ts
+++ app/events/create-event.component.ts
@@ -1,13 +1,45 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { EventService } from "./shared/events.service";
 
 @Component({
-    templateUrl: "app/events/create-event.component.html"
+    templateUrl: "app/events/create-event.component.html",
+     styles: [`
+        em { float: right; color: #E05C65; padding-left: 10px; }
+        .error input { background-color: #E3C3C5; }
+        .error ::-webkit-input-placeholder { color: #999; }
+        .error ::-moz-placeholder { color: #999; }
+        .error :-moz-placeholder { color: #999; }
+        .error :ms-input-placeholder { color: #999; }
+    `]
 })
-export class CreateEventComponent {
+export class CreateEventComponent implements OnInit {
     isDirty: boolean = true;
+    event: any;
 
-    constructor(private router: Router) {}
+    constructor(private router: Router, private eventService: EventService) {}
+
+    ngOnInit() {
+        this.event = {
+            name: "Ng Spectacular",
+            date: "8/8/2028",
+            time: "10am",
+            price: 799.99,
+            location: {
+                address: "456 Happy St",
+                city: "Felicity",
+                country: "Angularistan"
+            },
+            onlineUrl: "http://ngSpectacular.com",
+            imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5d/Pomegranate.jpg"
+        };
+    }
+
+    saveEvent(formValues) {
+        this.eventService.saveEvent(formValues);
+        this.isDirty = false;
+        this.router.navigate(['/events']);
+    }
 
     cancel() {
         this.router.navigate(['/events']);
