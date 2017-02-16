f756984ede7a34de87dd47ba44ee4b645ce34a50
lesnikovskiy <lesnikovski@gmail.com>
2017-02-16 23:43:12 +0200

    prepare diving deeper with reactive forms

--- app/events/create-event.component.ts
+++ app/events/create-event.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 import { EventService } from "./shared/events.service";
 
@@ -13,27 +13,11 @@ import { EventService } from "./shared/events.service";
         .error :ms-input-placeholder { color: #999; }
     `]
 })
-export class CreateEventComponent implements OnInit {
+export class CreateEventComponent {
     isDirty: boolean = true;
     event: any;
 
     constructor(private router: Router, private eventService: EventService) {}
-
-    ngOnInit() {
-        this.event = {
-            name: "Ng Spectacular",
-            date: "8/8/2028",
-            time: "10am",
-            price: 799.99,
-            location: {
-                address: "456 Happy St",
-                city: "Felicity",
-                country: "Angularistan"
-            },
-            onlineUrl: "http://ngSpectacular.com",
-            imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5d/Pomegranate.jpg"
-        };
-    }
 
     saveEvent(formValues) {
         this.eventService.saveEvent(formValues);
