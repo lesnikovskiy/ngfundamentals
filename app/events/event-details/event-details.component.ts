a71ad3cd27550e875d774c4d3d7dfbbe40441592
lesnikovskiy <lesnikovski@gmail.com>
2017-02-22 22:21:17 +0200

    sorting data

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -14,6 +14,7 @@ export class EventDetailsComponent implements OnInit {
     event: IEvent;
     addMode: boolean;
     filterBy: string = "all";
+    sortBy: string = "name";
 
     constructor(
         private eventService: EventService,
